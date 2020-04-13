c094c433ac8a4d1be6cad1acd39b943f917ed2e8
saad sufyan <saad.sufyan@loopsdigital.com>
2020-04-13 00:16:43 -0400

    login page redirect

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -8,7 +8,7 @@ export class AuthGuardService implements CanActivate {
   constructor(public auth: AuthService, public router: Router) {}
   canActivate(): boolean {
     if (!this.auth.isAuthenticated()) {
-      this.router.navigate(['']);
+      this.router.navigate(['login']);
       return false;
     }
     return true;
